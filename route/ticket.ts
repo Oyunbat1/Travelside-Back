3b8e375b6cdb763ad1707ac33d688a2012bf41a0
Oyunbat <oyunbat9958@gmail.com>
2025-04-24 12:26:50 +0800

    scheam checked

--- route/ticket.ts
+++ route/ticket.ts
@@ -1,5 +1,5 @@
 import express from "express";
 import { createTicket, getTickets } from "../controller/ticket";
 const ticketRouter = express.Router();
-ticketRouter.post("/create", createTicket).get("/", getTickets);
+ticketRouter.post("/create", createTicket).get("/get", getTickets);
 export default ticketRouter;
